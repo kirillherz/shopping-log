35d87e7f58d03eab18751aa761dce909f0a96ec3
Kirill <kirill.herz@gmail.com>
2018-10-26 15:23:50 +0300

    Add ProductAdmin model

--- log/admin.py
+++ log/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from log.models import Category
+from log.models import Category, Product
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
     pass
 
 

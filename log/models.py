cf27b50dc42fd06b01cec74d38a6c8f8460597b1
Kirill <kirill.herz@gmail.com>
2018-10-26 15:21:01 +0300

    Add Category model

--- log/models.py
+++ log/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    parent_category = models.ForeignKey(
+        'self', blank=True,  null=True,
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+

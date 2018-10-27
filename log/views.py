a8c7977690d255a3106e03076f6a8a6ca66380cf
Kirill <kirill.herz@gmail.com>
2018-10-28 00:04:32 +0300

    Add get_json view

--- log/views.py
+++ log/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.serializers import serialize
+from log.models import Day
 
 # Create your views here.
+def get_json(request):
+    if request.is_ajax():
+        data = serialize(
+            'json', Day.objects.all(), fields=('date', 'total'))
+        return HttpResponse(data)
+    else:
+        return HttpResponse("ERROR")
--- shopping_log/urls.py
+++ shopping_log/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from log.views import get_json
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get_json/', get_json)
 ]

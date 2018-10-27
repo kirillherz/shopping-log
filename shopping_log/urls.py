a13365376d166ae67968daf5728c957cd1256913
Kirill <kirill.herz@gmail.com>
2018-10-28 00:07:51 +0300

    Add import show_checks in urls.py

--- shopping_log/urls.py
+++ shopping_log/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from log.views import get_json
+from log.views import show_checks, get_json
 
 urlpatterns = [
     path('admin/', admin.site.urls),

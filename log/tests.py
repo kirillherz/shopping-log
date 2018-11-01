4054162cb5a07ed27def975ae97d372ffe5fde67
Kirill <kirill.herz@gmail.com>
2018-11-01 23:18:41 +0300

    Change test for create

--- log/tests.py
+++ log/tests.py
@@ -13,13 +13,15 @@ class ProductTestCase(TestCase):
         self.check.save()
         self.category = Category(name="test category", parent_category=None)
         self.category.save()
-
-    def test_create_product(self):
         self.product = Product(
             name="test product",
             cost=Decimal(15.0),
             checkFromShop=self.check,
             category=self.category)
         self.product.save()
-        self.assertEqual(self.check.total, 15.0)
+
+    def test_create_product(self):
+        product = Product.objects.get(name="test product")
+        check = product.checkFromShop
+        self.assertEqual(check.total, 15.0)
 

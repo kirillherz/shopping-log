7eb5d55bf6a8cbb9ce0c04a92c2575fbbad4071c
Kirill <kirill.herz@gmail.com>
2018-11-01 22:55:08 +0300

    Add test for create Product

--- log/tests.py
+++ log/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from log.models import Product, CheckFromShop, Shop, Category
+from django.utils.timezone import now
+from decimal import Decimal
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):
+
+    def setUp(self):
+        self.shop = Shop(name="test shop")
+        self.shop.save()
+        self.check = CheckFromShop(shop=self.shop, total=0, date=now())
+        self.check.save()
+        self.category = Category(name="test category", parent_category=None)
+        self.category.save()
+
+    def test_create_product(self):
+        prodcut = Product(
+            name="test product",
+            cost=Decimal(15.0),
+            checkFromShop=self.check,
+            category=self.category)
+        prodcut.save()
+        self.assertEqual(self.check.total, 15.0)
+

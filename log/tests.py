666a7106b257bbfe1c9ac0155b97ede35826bec7
Kirill <kirill.herz@gmail.com>
2018-11-02 13:04:34 +0300

    Change setUp in ProductTestCase

--- log/tests.py
+++ log/tests.py
@@ -7,18 +7,18 @@ from decimal import Decimal
 class ProductTestCase(TestCase):
 
     def setUp(self):
-        self.shop = Shop(name="test shop")
-        self.shop.save()
-        self.check = CheckFromShop(shop=self.shop, total=0, date=now())
-        self.check.save()
-        self.category = Category(name="test category", parent_category=None)
-        self.category.save()
-        self.product = Product(
+        shop = Shop(name="test shop")
+        shop.save()
+        check = CheckFromShop(shop=shop, total=0, date=now())
+        check.save()
+        category = Category(name="test category", parent_category=None)
+        category.save()
+        product = Product(
             name="test product",
             cost=Decimal(15.0),
-            checkFromShop=self.check,
-            category=self.category)
-        self.product.save()
+            checkFromShop=check,
+            category=category)
+        product.save()
 
     def test_create_product(self):
         product = Product.objects.get(name="test product")
